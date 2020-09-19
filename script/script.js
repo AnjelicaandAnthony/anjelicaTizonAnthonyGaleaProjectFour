2bb9353f36790ffa8de5dea4a20aaee0e8d360d6
anjelicatizon <anjelicatizon@gmail.com>
2020-09-19 16:16:31 -0400

    MVP

--- script/script.js
+++ script/script.js
@@ -10,22 +10,46 @@ foodAndBeer.getPairings = function(userChoice){
             food: userChoice
         }
     }).then((res) => {
-        console.log(res)
+        // console.log(res)
         $('ul').empty();
-        foodAndBeer.displayPairings(res) // ADD - CALL FUNCTION THAT DISPLAYS BEER PAIRINGS
+        foodAndBeer.displayPairings(res)
     })
 }
 
 
 foodAndBeer.displayPairings = function(pairing){
-    console.log(pairing)
-    pairing.forEach((beer)  => {
-    const title = $('<h2>').text(beer.name)
-    const description  = $('<p>').text(beer.food_pairing) 
-    const image =$('<img>').attr({"src":beer.image_url}) 
-    const displayedBeer = $('<li>').append(title, description, image)
-    $('ul').append(displayedBeer)
-    })
+    
+    for (let i = 0; i <= 2; i = i + 1) {
+        const title = $('<h2>').text(pairing[i].name)
+        const description  = $('<p>').text(pairing[i].food_pairing) 
+        const image =$('<img>').attr({"src":pairing[i].image_url}) 
+        const displayedBeer = $('<li>').append(title, description, image)
+
+        $('ul').append(displayedBeer)
+        // $('li').toggleClass()
+        
+        // $("#seeMore").click(function(e){
+        //     e.preventDefault();
+        //     $("div:hidden").slice(0,3).fadeIn("slow");
+            
+        //     if($("div:hidden").length == 0){
+        //         $("#seeMore").fadeOut("slow");
+        //     }
+        // });
+    }
+    
+    // pairing.forEach((beer)  => {
+    // const title = $('<h2>').text(beer.name)
+    // const description  = $('<p>').text(beer.food_pairing) 
+    // const image =$('<img>').attr({"src":beer.image_url}) 
+    // const displayedBeer = $('<li>').append(title, description, image)
+    
+    // const threeDisplayedBeers = displayedBeer.slice(0,2)
+    // console.log(threeDisplayedBeers)
+
+    // $('ul').append(displayedBeer)
+
+    // })
 }   
 
 
@@ -34,28 +58,17 @@ foodAndBeer.init = function() {
     $('.foodDropdown').on('change', function(event){
         event.preventDefault();
         const foodDropdown = $(this).val();
-        console.log(foodDropdown)
+        // console.log(foodDropdown)
         foodAndBeer.getPairings(foodDropdown);
     });
 
-    // $('.searchbar').on('submit', function(event){
-    //     console.log('HI YOU SEARCHED');
-    //     event.preventDefault();
-    //     const foodSearch= $(this).val();
-    //     console.log(foodSearch)
-    //     foodAndBeer.getPairings(foodSearch);
-    // });
-
     $('form').submit(function(event) {
-        console.log('HI YOU SEARCHED');
+        // console.log('HI YOU SEARCHED');
         event.preventDefault();
         const foodSearch= $('.searchbar').val();
-        console.log(foodSearch)
+        // console.log(foodSearch)
         foodAndBeer.getPairings(foodSearch);
       });
-
-    // 'input:text[class=‘searchbar’]'
-
 }
 
 // Document Ready
