bb64ccf28131f373a4c1a2ad4f4f27c6a0071845
anjelicatizon <anjelicatizon@gmail.com>
2020-09-20 10:48:45 -0400

    update JS with load more options

--- script/script.js
+++ script/script.js
@@ -16,56 +16,43 @@ foodAndBeer.getPairings = function(userChoice){
     })
 }
 
-
 foodAndBeer.displayPairings = function(pairing){
+    console.log(pairing.slice(0,2))
     
-    for (let i = 0; i <= 2; i = i + 1) {
-        const title = $('<h2>').text(pairing[i].name)
-        const description  = $('<p>').text(pairing[i].food_pairing) 
-        const image =$('<img>').attr({"src":pairing[i].image_url}) 
-        const displayedBeer = $('<li>').append(title, description, image)
+    // OG CODE
+    pairing.forEach((beer) => {
+        const beerName = $('<h2>').text(beer.name)
+        const pairing = $('<p>').text(`Goes well with: ${beer.food_pairing}`)
+        const image = $('<div>').append($('<img>').attr({'src': beer.image_url}))
 
+        const displayedBeer = $('<li>').append(beerName, pairing, image)
+
+        // appends each beer to the page - but doesn't display them - display:none is applied on the <li>'s
         $('ul').append(displayedBeer)
-        // $('li').toggleClass()
-        
-        // $("#seeMore").click(function(e){
-        //     e.preventDefault();
-        //     $("div:hidden").slice(0,3).fadeIn("slow");
-            
-        //     if($("div:hidden").length == 0){
-        //         $("#seeMore").fadeOut("slow");
-        //     }
-        // });     
-    }
+        $('.showMoreButton').show()
+        $('.restart').hide()
 
-    $('.showMoreButton').on('click', function(event){
-        event.preventDefault();
-        console.log('hello')
-        
-        for (let i = 3; i <= 5 ; i = i + 1) {
-            const title = $('<h2>').text(pairing[i].name)
-            const description  = $('<p>').text(pairing[i].food_pairing) 
-            const image =$('<img>').attr({"src":pairing[i].image_url}) 
-            const displayedBeer = $('<li>').append(title, description, image)
-    
-            $('ul').append(displayedBeer)
+        // targets the first 3 <li>'s and shows them
+        $('li').slice(0,3).show();
+    })
+
+    // When clicking load more button:
+        // - prevent default
+        // - get the next 3 pairings and fade in slow
+    // Has to go outside of the loop or else it will load them all
+    $(".showMoreButton").click(function(e){
+        e.preventDefault();
+        console.log('3 more')
+        $("li:hidden").slice(0,3).fadeIn("slow");
+    // There is a problem here because the console logs more than 1 click
+
+    // if the number of hidden <li>'s is 0, fade out the how more button
+    if($("li:hidden").length == 0){
+        $(".showMoreButton").fadeOut("slow");
+        $(".restart").fadeIn("slow");
         }
-    });   
-    
-    // pairing.forEach((beer)  => {
-    // const title = $('<h2>').text(beer.name)
-    // const description  = $('<p>').text(beer.food_pairing) 
-    // const image =$('<img>').attr({"src":beer.image_url}) 
-    // const displayedBeer = $('<li>').append(title, description, image)
-    
-    // const threeDisplayedBeers = displayedBeer.slice(0,2)
-    // console.log(threeDisplayedBeers)
-
-    // $('ul').append(displayedBeer)
-
-    // })
-}   
-
+    })  
+}
 
 //Functions that happen on page load
 foodAndBeer.init = function() {
